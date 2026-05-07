d1bf716873fb4e414f6a5af5ac2f0b27241f1505
Leonardo de Souza <leosouza5@hotmail.com.br>
2026-05-07 09:16:49 -0400

    Faltou add esses

--- public/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-import { maskCpf } from './cpf.js';
-import { initSigners } from './signers.js';
-import { initUpload } from './upload.js';
-
-window.maskCpf = maskCpf;
-
-initSigners();
-initUpload();
